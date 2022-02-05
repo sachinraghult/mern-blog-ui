3972988b51f77f29314e79f278785c58c34b6ddf
SACHIN RAGHUL T <sachinraghult2002@gmail.com>
2022-02-05 13:10:48 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // local API endpoint
-    baseURL: 'https://unofficial-blog-api.herokuapp.com/api/'
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://unofficial-blog-api.herokuapp.com/api/'
 })
 
 export default instance;
